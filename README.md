7299f99d90a38e988aac9cf7b3b7e634c569d609
Adam Hoyle <atomoil@gmail.com>
2015-11-06 17:41:58 +0000

    add the taginfo data to a google sheet

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # osm_taginfo
+
+A Simple script to get information from the Open Street Map TagInfo API and add it to a Google Sheet.
+
+Intended to be run as a cronjob on a daily basis.
+
+## Note:
+
+This script requires 2 files (next to this file) to work.
+
+* **google-generated-creds.json**
+	* See here for guide to making it - https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method
+* **google-sheet-id.json**
+	* This file must contain the id of the google sheet which the user in **google-generated-creds.json** must have write access to. See **google-sheet-id.example.json** for an example.
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+/**
+  * Note:
+  * This script requires a file called 'google-generated-creds.json' next to this file.
+  * See here for guide to making it - https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method
+  *
+  */
+
+// import everything
 var request = require('request')
 var moment  = require('moment')
-var gs      = require('google-spreadsheet')
+var gs      = require("google-spreadsheet")
+var g_creds = require('./google-generated-creds.json')
+var sheet_id = require('./google-sheet-id.json').google_sheet_id
+var g_sheet = new gs(sheet_id)
 
-request.get('http://taginfo.openstreetmap.org.uk/api/4/tag/stats?key=leisure&value=nature_reserve', function (error, response, body) {
+
+// what are we looking for?
+var tag = 'leisure:nature_reserve'
+var key = 'leisure'
+var value = 'nature_reserve'
+
+
+request.get('http://taginfo.openstreetmap.org.uk/api/4/tag/stats?key='+key+'&value='+value, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // example response: {"total":4,"url":"http://taginfo.openstreetmap.org.uk/api/4/tag/stats?key=leisure&value=nature_reserve","data":[{"type":"all","count":1670,"count_fraction":0.0},{"type":"nodes","count":147,"count_fraction":0.0},{"type":"ways","count":1454,"count_fraction":0.0002},{"type":"relations","count":69,"count_fraction":0.0005}]}
         // convert json string to object
@@ -36,8 +54,21 @@ request.get('http://taginfo.openstreetmap.org.uk/api/4/tag/stats?key=leisure&val
         	
         	}
         }
-        console.log( "we have",date_str,all,nodes,ways,relations)
+        
+        // now initialise the Google Sheet Auth
+		g_sheet.useServiceAccountAuth(g_creds, function(err){
+			
+			// add a row to the first worksheet in the google sheet
+			g_sheet.addRow( 1, {date:date_str, tag:tag, all:all, nodes:nodes, ways:ways, relations:relations}, function(err){
+				if (err){
+					console.log("Sorry, there was an error adding to the google sheet",err)
+				} else {
+					console.log("Added to google sheet")
+				}
+			})
+		})
+        
     } else {
-    	console.log("there was an error - ", error)
+    	console.log("Sorry, there was an error getting taginfo data", error)
     }
 })
